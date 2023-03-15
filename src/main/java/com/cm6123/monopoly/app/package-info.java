a852e9eabbe7483eb47c01f825d7d54d7160bf06
Carl Jones <jonesc162@cardiff.ac.uk>
2023-03-15 15:19:18 +0000

    Updated package structure to make separation for testing clearer.

--- src/main/java/com/cm6123/monopoly/app/package-info.java
+++ src/main/java/com/cm6123/monopoly/app/package-info.java
@@ -1,4 +1,6 @@
 /**
  * Contains console classes for the user interface.
+ * You do not need to write automated tests of classes/methods in this package.
+ * DO NOT HIDE business logic in this package.
  */
 package com.cm6123.monopoly.app;
---
+++ src/main/java/com/cm6123/monopoly/game/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains the main game logic.
+ * This is the package root that should be subjected to automated testing.
+ * It should not have any user interface code in it.
+ */
+package com.cm6123.monopoly.game;
