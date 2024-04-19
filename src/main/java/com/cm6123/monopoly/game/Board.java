207179515773e9af510dfc4381b95e8dce4fca72
Antonio Cheong <teapotv8@proton.me>
2024-04-19 14:53:41 +0100

    format

--- src/main/java/com/cm6123/monopoly/game/Board.java
+++ src/main/java/com/cm6123/monopoly/game/Board.java
@@ -3,50 +3,51 @@ package com.cm6123.monopoly.game;
 import java.util.Random;
 
 public class Board {
-	private Space[] spaces;
+  private Space[] spaces;
 
-	public Board(Integer size) {
-		this.spaces = new Space[size];
-		this.spaces[0] = new Space(Spaces.HOME);
-		Random rand = new Random();
-		for (Integer i = 1; i < size; i++) {
-			// Get random space type
-			this.spaces[i] = new Space(Spaces.values()[rand.nextInt(Spaces.values().length)]);
-		}
-	}
+  public Board(Integer size) {
+    this.spaces = new Space[size];
+    this.spaces[0] = new Space(Spaces.HOME);
+    Random rand = new Random();
+    for (Integer i = 1; i < size; i++) {
+      // Get random space type
+      this.spaces[i] = new Space(Spaces.values()[rand.nextInt(Spaces.values().length)]);
+    }
+  }
 
-	public Space space(Integer index) {
-		return this.spaces[index];
-	}
+  public Space space(Integer index) {
+    return this.spaces[index];
+  }
 
-	public Space[] spaces() {
-		return this.spaces;
-	}
+  public Space[] spaces() {
+    return this.spaces;
+  }
 }
+
 enum Target {
-	BANKER,
-	PLAYER
+  BANKER,
+  PLAYER
 }
 
 class Payment {
-	private Integer amount;
-	private Target target;
+  private Integer amount;
+  private Target target;
 
-	public Payment(Integer amount) {
-		this.amount = amount;
-		this.target = Target.BANKER;
-	}
+  public Payment(Integer amount) {
+    this.amount = amount;
+    this.target = Target.BANKER;
+  }
 
-	public Payment(Integer amount, Target target) {
-		this.target = target;
-		this.amount = amount;
-	}
+  public Payment(Integer amount, Target target) {
+    this.target = target;
+    this.amount = amount;
+  }
 
-	public Integer amount() {
-		return this.amount;
-	}
+  public Integer amount() {
+    return this.amount;
+  }
 
-	public Target target() {
-		return this.target;
-	}
+  public Target target() {
+    return this.target;
+  }
 }
