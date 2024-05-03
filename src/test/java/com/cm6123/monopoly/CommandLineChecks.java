ec7e824a1e38f6e5c8e249b914a95b211be64445
Antonio Cheong <acheong@duti.dev>
2024-05-03 11:29:28 +0100

    make UserInterface private since it's not really used outside of the app package

--- src/test/java/com/cm6123/monopoly/CommandLineChecks.java
+++ src/test/java/com/cm6123/monopoly/CommandLineChecks.java
@@ -3,7 +3,6 @@ package com.cm6123.monopoly;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.cm6123.monopoly.app.CommandLineInterface;
-import com.cm6123.monopoly.app.UserInterface;
 import com.cm6123.monopoly.game.Board;
 import com.cm6123.monopoly.game.Player;
 import java.io.ByteArrayInputStream;
@@ -18,7 +17,7 @@ public class CommandLineChecks {
   void testChoice() {
     String input = "-1\n2\nhello\n0.3\n";
     InputStream buf = new ByteArrayInputStream(input.getBytes());
-    UserInterface ui = new CommandLineInterface(buf);
+    CommandLineInterface ui = new CommandLineInterface(buf);
     String[] choices = {"Choice 1", "Choice 2", "Choice 3"};
     assertEquals(ui.promptForChoice(choices), 2);
     assertEquals(ui.getDouble(""), 0.3);
@@ -28,7 +27,7 @@ public class CommandLineChecks {
   @ValueSource(ints = {10, 24, 35, 41, 48, 50})
   void testBoardRendering(int size) {
     Board board = new Board(size, new Player[0]);
-    UserInterface ui = new CommandLineInterface(null);
+    CommandLineInterface ui = new CommandLineInterface(null);
     ui.renderBoard(board);
   }
 }
